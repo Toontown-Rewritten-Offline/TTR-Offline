f5db0a60dce67c5eb88ad6d02652cd3edfb26758
Drew Rogers <regdogg.acr@gmail.com>
2022-04-15 11:14:55 -0500

    Remove "Logout", align "Quit" to Bottom Right

--- toontown/login/AvatarChooser.py
+++ toontown/login/AvatarChooser.py
@@ -55,8 +55,8 @@ class AvatarChooser(StateData.StateData):
         base.disableMouse()
         self.title.reparentTo(aspect2d)
         self.quitButton.show()
-        if base.cr.loginInterface.supportsRelogin():
-            self.logoutButton.show()
+        #if base.cr.loginInterface.supportsRelogin():
+        #    self.logoutButton.show()
         self.pickAToonBG.setBin('background', 1)
         self.pickAToonBG.reparentTo(aspect2d)
         base.setBackgroundColor(Vec4(0.145, 0.368, 0.78, 1))
@@ -95,12 +95,12 @@ class AvatarChooser(StateData.StateData):
         self.pickAToonBG.setScale(1, 1, 1)
         self.title = OnscreenText(TTLocalizer.AvatarChooserPickAToon, scale=TTLocalizer.ACtitle, parent=hidden, font=ToontownGlobals.getSignFont(), fg=(1, 0.9, 0.1, 1), pos=(0.0, 0.82))
         quitHover = gui.find('**/QuitBtn_RLVR')
-        self.quitButton = DirectButton(image=(quitHover, quitHover, quitHover), relief=None, text=TTLocalizer.AvatarChooserQuit, text_font=ToontownGlobals.getSignFont(), text_fg=(0.977, 0.816, 0.133, 1), text_pos=TTLocalizer.ACquitButtonPos, text_scale=TTLocalizer.ACquitButton, image_scale=1, image1_scale=1.05, image2_scale=1.05, scale=1.05, pos=(1.08, 0, -0.907), command=self.__handleQuit)
-        self.logoutButton = DirectButton(relief=None, image=(quitHover, quitHover, quitHover), text=TTLocalizer.OptionsPageLogout, text_font=ToontownGlobals.getSignFont(), text_fg=(0.977, 0.816, 0.133, 1), text_scale=TTLocalizer.AClogoutButton, text_pos=(0, -0.035), pos=(-1.17, 0, -0.914), image_scale=1.15, image1_scale=1.15, image2_scale=1.18, scale=0.5, command=self.__handleLogoutWithoutConfirm)
+        self.quitButton = DirectButton(image=(quitHover, quitHover, quitHover), parent=base.a2dBottomRight, relief=None, text=TTLocalizer.AvatarChooserQuit, text_font=ToontownGlobals.getSignFont(), text_fg=(0.977, 0.816, 0.133, 1), text_pos=TTLocalizer.ACquitButtonPos, text_scale=TTLocalizer.ACquitButton, image_scale=1, image1_scale=1.05, image2_scale=1.05, scale=1.05, pos=(-0.25, 0, 0.1), command=self.__handleQuit)
+        #self.logoutButton = DirectButton(relief=None, image=(quitHover, quitHover, quitHover), text=TTLocalizer.OptionsPageLogout, text_font=ToontownGlobals.getSignFont(), text_fg=(0.977, 0.816, 0.133, 1), text_scale=TTLocalizer.AClogoutButton, text_pos=(0, -0.035), pos=(-1.17, 0, -0.914), image_scale=1.15, image1_scale=1.15, image2_scale=1.18, scale=0.5, command=self.__handleLogoutWithoutConfirm)
         if ConfigVariableBool('want-new-ttrloader', False):
             self.loading.cleanup()
             self.loading.musicLoadIn()
-        self.logoutButton.hide()
+        #self.logoutButton.hide()
         gui.removeNode()
         gui2.removeNode()
         newGui.removeNode()
@@ -219,8 +219,8 @@ class AvatarChooser(StateData.StateData):
         del self.title
         self.quitButton.destroy()
         del self.quitButton
-        self.logoutButton.destroy()
-        del self.logoutButton
+        #self.logoutButton.destroy()
+        #del self.logoutButton
         self.pickAToonBG.removeNode()
         del self.pickAToonBG
         del self.avatarList
