6eef18b786387295d3333fe26e8a3d3437ef4b4f
Drew Rogers <regdogg.acr@gmail.com>
2022-05-14 11:07:24 -0500

    Fix cog gallery crash

--- toontown/shtiker/SuitPage.py
+++ toontown/shtiker/SuitPage.py
@@ -470,7 +470,7 @@ class SuitPage(ShtikerPage.ShtikerPage):
         elif status == COG_COMPLETE1:
             panel['image_color'] = PANEL_COLORS_COMPLETE1[index / SuitDNA.suitsPerDept]
         elif status == COG_COMPLETE2:
-            panel['image_color'] = PANEL_COLORS_COMPLETE2[index / SuitDNA.suitsPerDept]
+            panel['image_color'] = PANEL_COLORS_COMPLETE2[index // SuitDNA.suitsPerDept]
 
     def updateAllCogs(self, status):
         for index in range(0, len(base.localAvatar.cogs)):
