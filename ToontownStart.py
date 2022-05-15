311f30599fd9cfaca428e25afa2fa6ec9ab90179
Drew Rogers <regdogg.acr@gmail.com>
2022-05-15 16:13:40 -0500

    Fix retro-rewritten

--- ToontownStart.py
+++ ToontownStart.py
@@ -1,12 +1,12 @@
 from panda3d.core import ConfigVariableBool, loadPrcFile
 
-loadPrcFile("config/dev.prc")
+loadPrcFile('config/dev.prc')
 
 from toontown.settings.ToontownSettings import ToontownSettings
 settings = ToontownSettings()
 settings.loadFromSettings()
 
-if ConfigVariableBool('want-new-ttrloader', False):
-    import toontown.toonbase.ToontownStartNEW
+if ConfigVariableBool('want-retro-rewritten', False):
+    from toontown.toonbase import ToontownStartOLD
 else:
-    import toontown.toonbase.ToontownStartOLD
+    from toontown.toonbase import ToontownStartNEW
--- toontown/toonbase/ToontownStart.py
+++ toontown/toonbase/ToontownStart.py
@@ -1,5 +1,9 @@
 from panda3d.core import ConfigVariableBool
 
+from toontown.settings.ToontownSettings import ToontownSettings
+settings = ToontownSettings()
+settings.loadFromSettings()
+
 if ConfigVariableBool('want-retro-rewritten', False):
     from . import ToontownStartOLD
 else:
