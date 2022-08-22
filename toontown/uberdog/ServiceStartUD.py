dce53b52d21b3504a269809fe0f31cae91135ee2
Drew Rogers <regdogg.acr@gmail.com>
2022-08-21 22:49:13 -0500

    Fix AI and UD start.bat and arguments

--- toontown/uberdog/ServiceStartUD.py
+++ toontown/uberdog/ServiceStartUD.py
@@ -8,24 +8,23 @@ import argparse
 os.makedirs('astron/databases/astrondb', exist_ok = True) 
 os.makedirs('astron/mongo/astrondb', exist_ok = True) 
 
-''' WIP
-# Arguments
-parser = argparse.ArgumentParser()
-parser.add_argument('--base-channel', help='The base channel that the server may use.')
-parser.add_argument('--max-channels', help='The number of channels the server may use.')
-parser.add_argument('--stateserver', help="The control channel of this UD's designated State Server.")
-parser.add_argument('--astron-ip', help="The IP address of the Astron Message Director to connect to.")
-parser.add_argument('--eventlogger-ip', help="The IP address of the Astron Event Logger to log to.")
-args = parser.parse_args()
+if not ConfigVariableBool('auto-start-server', False):
+    # Arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--base-channel', help='The base channel that the server may use.')
+    parser.add_argument('--max-channels', help='The number of channels the server may use.')
+    parser.add_argument('--stateserver', help="The control channel of this UD's designated State Server.")
+    parser.add_argument('--astron-ip', help="The IP address of the Astron Message Director to connect to.")
+    parser.add_argument('--eventlogger-ip', help="The IP address of the Astron Event Logger to log to.")
+    args = parser.parse_args()
 
-localconfig = ''
-if args.base_channel: localconfig += 'air-base-channel %s\n' % args.base_channel
-if args.max_channels: localconfig += 'air-channel-allocation %s\n' % args.max_channels
-if args.stateserver: localconfig += 'air-stateserver %s\n' % args.stateserver
-if args.astron_ip: localconfig += 'air-connect %s\n' % args.astron_ip
-if args.eventlogger_ip: localconfig += 'eventlog-host %s\n' % args.eventlogger_ip
-loadPrcFileData('Command-line', localconfig)
-WIP '''
+    localconfig = ''
+    if args.base_channel: localconfig += 'air-base-channel %s\n' % args.base_channel
+    if args.max_channels: localconfig += 'air-channel-allocation %s\n' % args.max_channels
+    if args.stateserver: localconfig += 'air-stateserver %s\n' % args.stateserver
+    if args.astron_ip: localconfig += 'air-connect %s\n' % args.astron_ip
+    if args.eventlogger_ip: localconfig += 'eventlog-host %s\n' % args.eventlogger_ip
+    loadPrcFileData('Command-line', localconfig)
 
 if __debug__:
     loadPrcFile('config/dev.prc')
