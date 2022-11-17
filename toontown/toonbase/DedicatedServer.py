8438e1d4e664bc2d01e923e9a29c15fb2f40ee37
unknown <regdogg.acr@gmail.com>
2022-11-17 10:37:57 -0600

    Fixed the Static astrond for Linux

--- toontown/toonbase/DedicatedServer.py
+++ toontown/toonbase/DedicatedServer.py
@@ -64,7 +64,7 @@ class DedicatedServer:
             self.astronProcess = subprocess.Popen('astron/astrondmac --loglevel info %s' % astronConfig,
                                                   stdin=self.astronLog, stdout=self.astronLog, stderr=self.astronLog, shell=True)
         elif sys.platform in ['linux', 'linux2']:
-            self.astronProcess = subprocess.Popen('astron/astrond --loglevel info %s' % astronConfig,
+            self.astronProcess = subprocess.Popen('astron/astrondlinux --loglevel info %s' % astronConfig,
                                                   stdin=self.astronLog, stdout=self.astronLog, stderr=self.astronLog)
 
     def startAstronYAML(self, task):
