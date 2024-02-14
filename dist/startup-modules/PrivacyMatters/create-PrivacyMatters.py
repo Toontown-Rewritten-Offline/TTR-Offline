dfbe07b5247e46ebae8cc0f5bb9dd57cfc7f3cfa
Drew Rogers <regdogg.acr@gmail.com>
2024-02-14 14:01:04 -0600

    Update tools for Distribution

--- dist/startup-modules/PrivacyMatters/create-PrivacyMatters.py
+++ dist/startup-modules/PrivacyMatters/create-PrivacyMatters.py
@@ -1,9 +1,9 @@
-configfile = open('private_client.prc', 'r')
+configfile = open('../../../config/private_client.prc', 'r')
 configlines = configfile.read()
 configlines = configlines.encode('utf-8')
 data = 'CONFIGFILE = %s' % configlines + '\n'
 
-dcfile = open('ttrp.dc', 'r')
+dcfile = open('../../../config/ttrp.dc', 'r')
 dclines = dcfile.read()
 dclines = dclines.encode('utf-8')
 data += 'DCFILE = %s' % dclines
