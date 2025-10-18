cf48437e4ac9055474bd53c22d6960779afb9b29
Drew Rogers <regdogg.acr@gmail.com>
2025-10-18 08:52:50 -0500

    Remove unused module.

--- toontown/toonfest/PrizeScreen.py
+++
@@ -1,8 +0,0 @@
-from direct.gui.DirectGui import *
-from direct.directnotify import DirectNotifyGlobal
-
-class PrizeScreen(DirectFrame):
-    notify = DirectNotifyGlobal.directNotify.newCategory('PrizeScreen')
-
-    def __init__(self, parent = aspect2d, prizes=False, **kw):
-        return
